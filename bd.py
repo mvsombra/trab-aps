791ad07b1f65b9a8ae4c4a58b4f19903ae3a886c
Marcos Sombra <markos.vs@live.com>
2019-05-10 10:45:00 -0300

    umas parada

--- bd.py
+++ bd.py
@@ -12,4 +12,26 @@ class BancoDados:
 
 class AcessoBD:
     def __init__(self):
-        self.bd = BancoDados()
+        self.db = BancoDados()
+
+    def select_users(self, nome=None, email=None, max_results=0):
+        q = "SELECT * FROM usuario {}ORDER BY nome;"
+        if(email):
+            q = q.format("WHERE email='{}' ".format(email))
+        elif(nome):
+            q = q.format("WHERE nome='{}' ".format(nome))
+        else:
+            q = q.format('')
+        # executa a busca
+        self.db.cur.execute(q)
+        # limita os resultados e retorna
+        if(max_results == 1):
+            return self.db.cur.fetchall()[0]
+        elif(max_results > 1):
+            return self.db.cur.fetchall()[:max_results]
+        else:
+            return self.db.cur.fetchall()
+
+    def auth_user(self, email, senha):
+        senha_user = self.select_users(email=email, max_results=1)[6]
+        return senha == senha_user
--- server.py
+++ server.py
@@ -1,7 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, g, session, redirect, url_for
+from sessions import RedisSessionInterface
 from desenha_tela import DesenhaTela as dt
+from bd import AcessoBD
 
+dba = AcessoBD()
 app = Flask(__name__)
+app.session_interface = RedisSessionInterface()
 draw = dt()
 
 
@@ -28,14 +32,27 @@ def notificacao():
     return "Notificação atividada"
 
 
-@app.route('/entrar')
+@app.route('/entrar', methods=['GET', 'POST'])
 def entrar():
+    if(g.user):
+        return redirect(url_for('index'))
+
+    if(request.method == 'POST'):
+        session.pop('user', None)
+        return str(dba.auth_user(request.form['email'], request.form['senha']))
     return draw.render('login')
 
 
 @app.route('/cadastrar')
 def cadastrar():
     return draw.render('cadastro')
+
+
+@app.before_request
+def before_request():
+    g.user = None
+    if('user' in session):
+        g.user = session['user']
 
 
 if __name__ == '__main__':
